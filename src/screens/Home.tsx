2ca9b19c303f50a67d3db6359c59c777bc40f526
rstubryan <restubumirr@gmail.com>
2024-06-29 13:20:00 +0700

    feat(+): update movie screens

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,25 +1,56 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
+import { ScrollView, View, StatusBar, StyleSheet } from "react-native";
+import type { MovieListProps } from "../types/app";
+import MovieList from "../components/movies/MoviesList";
 
-type RootStackParamList = {
-  Home: undefined;
-  MovieDetail: undefined;
+const movieLists: MovieListProps[] = [
+  {
+    title: "Now Playing in Theater",
+    path: "movie/now_playing?language=en-US&page=1",
+    coverType: "backdrop",
+  },
+  {
+    title: "Upcoming Movies",
+    path: "movie/upcoming?language=en-US&page=1",
+    coverType: "poster",
+  },
+  {
+    title: "Top Rated Movies",
+    path: "movie/top_rated?language=en-US&page=1",
+    coverType: "poster",
+  },
+  {
+    title: "Popular Movies",
+    path: "movie/popular?language=en-US&page=1",
+    coverType: "poster",
+  },
+];
+
+const Home = (): JSX.Element => {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        {movieLists.map((movieList) => (
+          <MovieList
+            title={movieList.title}
+            path={movieList.path}
+            coverType={movieList.coverType}
+            key={movieList.title}
+          />
+        ))}
+        <StatusBar translucent={false} />
+      </View>
+    </ScrollView>
+  );
 };
 
-type NavigationProp = StackNavigationProp<RootStackParamList, "Home">;
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight ?? 32,
+    alignItems: "center",
+    justifyContent: "center",
+    rowGap: 16,
+  },
+});
 
-export default function Home() {
-  const navigation = useNavigation<NavigationProp>();
-
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home</Text>
-      <Button
-        title="Go to Movie Detail"
-        onPress={() => navigation.navigate("MovieDetail")}
-      />
-    </View>
-  );
-}
+export default Home;
