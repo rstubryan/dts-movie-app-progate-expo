4130c21f24f3d265ae1940db1e701ce802c7f795
rstubryan <restubumirr@gmail.com>
2024-06-30 11:12:35 +0700

    feat(category): add select picker, wip on viewing movie by category

--- src/components/search/CategorySearch.tsx
+++ src/components/search/CategorySearch.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { View, TextInput, FlatList, Text } from "react-native";
+import { View, StyleSheet, FlatList, Alert } from "react-native";
 import { API_ACCESS_TOKEN } from "@env";
+import { Picker } from "@react-native-picker/picker";
+import MovieItem from "../movies/MovieItem";
+import type { Movie } from "../../types/app";
 
 interface Category {
   id: number;
@@ -9,6 +12,8 @@ interface Category {
 
 const CategorySearch = () => {
   const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
     const getCategories = async () => {
@@ -34,15 +39,61 @@ const CategorySearch = () => {
     getCategories();
   }, []);
 
+  const getMoviesByGenre = async (genreId: string) => {
+    const url = `https://api.themoviedb.org/3/discover/movie?with_genres=${genreId}`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    };
+
+    fetch(url, options)
+      .then(async (response) => {
+        const data = await response.json();
+        setMovies(data.results);
+      })
+      .catch((errorResponse) => {
+        console.log(errorResponse);
+      });
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
+      <Picker
+        selectedValue={selectedCategory}
+        onValueChange={(itemValue) => {
+          setSelectedCategory(itemValue);
+          getMoviesByGenre(itemValue);
+          Alert.alert(`Selected genre id is: ${itemValue}`); // Menampilkan id genre yang dipilih
+        }}
+      >
+        {categories.map((item) => (
+          <Picker.Item key={item.id} label={item.name} value={item.id} />
+        ))}
+      </Picker>
       <FlatList
-        data={categories}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
+        data={movies}
+        renderItem={({ item }) => (
+          <MovieItem
+            movie={item}
+            size={{ width: 100, height: 160 }}
+            coverType="poster"
+          />
+        )}
+        keyExtractor={(item: Movie) => item.id.toString()}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginTop: 28,
+  },
+});
 
 export default CategorySearch;
