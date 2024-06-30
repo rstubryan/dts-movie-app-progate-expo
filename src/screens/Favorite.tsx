170b21b63fe52384decc518b9201846102bdf714
rstubryan <restubumirr@gmail.com>
2024-06-30 11:34:36 +0700

    feat(favorite): add favorite screen

--- src/screens/Favorite.tsx
+++ src/screens/Favorite.tsx
@@ -1,15 +1,47 @@
 import React, { useState, useCallback } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, FlatList, TouchableOpacity, Text } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useFocusEffect } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { API_ACCESS_TOKEN } from "@env";
+import MovieItem from "../components/movies/MovieItem";
+import type { Movie } from "../types/app";
+
+type NavigateFunction = (routeName: string, params: { id: number }) => void;
+
+type CoverType = "poster" | "backdrop";
+
+type MovieItemWithPressProps = {
+  movie: Movie;
+  size: { width: number; height: number; marginBottom: number };
+  coverType: CoverType;
+};
 
 export default function Favorite() {
-  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [favoriteMovies, setFavoriteMovies] = useState<Movie[]>([]);
+  const navigation = useNavigation();
 
   const getFavoriteMovies = useCallback(async () => {
     const favoriteList =
       JSON.parse((await AsyncStorage.getItem("@FavoriteList")) as string) || [];
-    setFavoriteMovies(favoriteList);
+
+    const favoriteMovies = await Promise.all(
+      favoriteList.map(async (movieId: number) => {
+        const url = `https://api.themoviedb.org/3/movie/${movieId}`;
+        const options = {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+          },
+        };
+
+        const response = await fetch(url, options);
+        const movie = await response.json();
+        return movie;
+      }),
+    );
+
+    setFavoriteMovies(favoriteMovies);
   }, []);
 
   useFocusEffect(
@@ -18,14 +50,63 @@ export default function Favorite() {
     }, [getFavoriteMovies]),
   );
 
+  const MovieItemWithPress: React.FC<MovieItemWithPressProps> = ({
+    movie,
+    size,
+    coverType,
+  }) => (
+    <TouchableOpacity
+      onPress={() => {
+        (navigation.navigate as NavigateFunction)("MovieDetail", {
+          id: movie.id,
+        });
+      }}
+    >
+      <View pointerEvents="none">
+        <MovieItem
+          movie={movie as Movie}
+          size={size}
+          coverType={coverType as CoverType}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
-    <View>
-      <Text>Favorite Movies:</Text>
-      <FlatList
-        data={favoriteMovies}
-        keyExtractor={(item: number) => item.toString()}
-        renderItem={({ item }: { item: number }) => <Text>{item}</Text>}
-      />
+    <View style={{ marginTop: 64, margin: 16, flex: 1 }}>
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: "bold",
+          marginBottom: 16,
+          textAlign: "center",
+        }}
+      >
+        Favorite Movies
+      </Text>
+      {favoriteMovies.length === 0 ? (
+        <View
+          style={{ justifyContent: "center", alignItems: "center", flex: 1 }}
+        >
+          <Text style={{ textAlign: "center" }}>
+            You have no favorite movies yet.
+          </Text>
+        </View>
+      ) : (
+        <FlatList
+          contentContainerStyle={{ paddingBottom: 280 }}
+          numColumns={3}
+          data={favoriteMovies}
+          renderItem={({ item }) => (
+            <MovieItemWithPress
+              movie={item}
+              size={{ width: 117, height: 160, marginBottom: 4 }} // Adjust size as needed
+              coverType="poster" // Adjust coverType as needed
+            />
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      )}
     </View>
   );
 }
